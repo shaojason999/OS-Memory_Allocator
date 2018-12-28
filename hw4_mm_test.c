d10b1f0e88dbf97c6d786e680131cdf46f87332f
shaojason999 <shaojason999@gmail.com>
2018-12-29 02:25:21 +0800

    Handle the datatype

--- hw4_mm_test.c
+++ hw4_mm_test.c
@@ -2,25 +2,63 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
 void *start_brk;
+struct BIN {
+    void *prev;
+    void *nxt;
+} bin[11];
+void init()
+{
+    int i;
+    for(i=0; i<11; ++i) {
+        bin[i].prev=NULL;
+        bin[i].nxt=NULL;
+    }
+//	*(long int*)start_brk=0x9a82b3193823;
+//	printf("%p %p\n",start_brk,*(long int*)start_brk);
+    *(long int*)start_brk=start_brk+32*1024;	//prev chunk
+    *(long int*)(start_brk+8)=start_brk+32*1024;	//next chunk;
+    *(long int*)(start_brk+32*1024)=start_brk;
+    *(long int*)(start_brk+32*1024+8)=start_brk;
+    *(int*)(start_brk+16)=32*1024<<1+0;	//31 bits for size(<<1) + 1 bit for allocated flag(0 for free)
+    *(int*)(start_brk+20)=32*1024<<1+0;	//31 bits for size(<<1) + 1 bit for mmap flag(0 for not mmap)
+    *(int*)(start_brk+32*1024+16)=32*1024<<1+0;	//31 bits for size(<<1) + 1 bit for allocated flag(0 for free)
+    *(int*)(start_brk+32*1024+20)=32*1024<<1+0;	//31 bits for size(<<1) + 1 bit for mmap flag(0 for not mmap)
+
+    printf("%p 0x%lx\n",start_brk,*(long int*)start_brk);
+//	printf("%p %p\n",start_brk,*(long int*)start_brk);
+
+}
 
 int main(int argc, char *argv[])
 {
     start_brk=sbrk(64*1024);
-    int *temp=(int*)start_brk;
-    *(int*)start_brk=10;
-    printf("%d\n",*temp);
-    int *temp1=(int*)start_brk;
-    printf("%d\n",*temp1);
-    temp++;
-    *temp=2;
-    printf("%d\n",*temp);
-    printf("%p\n",start_brk);
-    start_brk+=4;
-    printf("%p\n",start_brk);
-    int *temp2=(int*)start_brk;
-    printf("%d\n",*temp2);
-    printf("%d\n",sizeof(int));
+    init();
+    int size,result;
+    char input[10],bin_or_mmap[20];
+    void *address;
+    while(1) {
+        scanf("%s",input);
+        switch(input[0]) {
+        case 'a':
+            scanf("%d",&size);
+            address=hw_malloc(size);
+            break;
+        case 'f':
+            scanf("%p",&address);
+            //printf("%p\n",address);
+            result=hw_free(address);
+            if(result==1)
+                printf("success\n");
+            else
+                printf("fail\n");
+            break;
+        case 'p':
+            scanf("%s",bin_or_mmap);
+            break;
+        }
 
+    }
     return 0;
 }
--- lib/hw_malloc.c
+++ lib/hw_malloc.c
@@ -1,9 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct BIN {
+    void *prev;
+    void *nxt;
+};
+
+extern struct BIN bin[11];
+extern void* start_brk;
 
 void *hw_malloc(size_t bytes)
 {
+    printf("%p\n",bin[0].prev);
     return NULL;
 }
 
